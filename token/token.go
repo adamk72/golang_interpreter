0ab45a28ad436a22c9ef72bb14efbdcda7761eaa
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-07-25 19:14:09 +0200

    add more tokens from the docs

--- token/token.go
+++ token/token.go
@@ -3,13 +3,18 @@ package token
 type TokenType string
 
 type Token struct {
-	Type TokenType
+	Type    TokenType
 	Literal string
 }
 
-var keywords = map[string]TokenType {
-	"fn": FUNCTION,
-	"let": LET,
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
@@ -22,18 +27,42 @@ func LookupIdent(ident string) TokenType {
 // Q: how comes this doesn't have a name? How do we ensure that when it's updated, the rest of the code is aware of that (for tokens)?
 // A: because these constants are part of this package. Refer to them via `token.EOF` for example.
 const (
-	ILLEGAL		= "ILLEGAL"
-	EOF 			= "EOF"
-	IDENT			= "IDENT"
-	INT				= "INT"
-	ASSIGN		= "="
-	PLUS			= "+"
-	COMMA			= ","
-	SEMICOLON	= ";"
-	LPAREN		= "("
-	RPAREN		= ")"
-	LBRACE		= "{"
-	RBRACE		= "}"
-	FUNCTION	= "FUNCTION"
-	LET				= "LET"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
