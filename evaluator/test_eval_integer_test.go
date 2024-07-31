3c2a87db5e013919c319e5675a950e529f545ae1
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-07-31 18:52:51 +0200

    add minus prefix (p119)

--- evaluator/test_eval_integer_test.go
+++ evaluator/test_eval_integer_test.go
@@ -7,12 +7,15 @@ import (
 
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
-		input string
+		input    string
 		expected int64
-	} {
+	}{
 		{"5", 5},
 		{"10", 10},
-		
+		{"5", 5},
+		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 
 	for _, tt := range tests {
@@ -21,12 +24,16 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
-func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool { result, ok := obj.(*object.Integer)
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
 	if !ok {
-						 t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
-	return false }
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
 	if result.Value != expected {
-	t.Errorf("object has wrong value. got=%d, want=%d",
-								 result.Value, expected)
-	return false }
-	return true }
+		t.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+		return false
+	}
+	return true
+}
