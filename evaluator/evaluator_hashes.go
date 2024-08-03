1a370ecabe513b89ce97d93836b452fa00c919a6
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-08-03 16:49:29 +0200

    add hash index support (p197)

--- evaluator/evaluator_hashes.go
+++ evaluator/evaluator_hashes.go
@@ -25,3 +25,16 @@ func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Obje
 	}
 	return &object.Hash{Pairs: pairs}
 }
+
+func evalHashIndexExpression(hash, index object.Object) object.Object {
+	hashObject := hash.(*object.Hash)
+	key, ok := index.(object.Hashable)
+	if !ok {
+		return newError("unusable as hash key: %s", index.Type())
+	}
+	pair, ok := hashObject.Pairs[key.HashKey()]
+	if !ok {
+		return NULL
+	}
+	return pair.Value
+}
