179390e3db0a0d0366f082fe6edb0dcfdc328c0b
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-08-03 16:27:06 +0200

    add hash basics

--- object/object.go
+++ object/object.go
@@ -9,6 +9,10 @@ import (
 
 type ObjectType string
 type BuiltinFunction func(args ...Object) Object
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
 
 const (
 	INTEGER_OBJ      = "INTEGER"
@@ -20,6 +24,7 @@ const (
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
 	ARRAY_OBJ        = "ARRAY"
+	HASH_OBJ         = "HASH"
 )
 
 type Object interface {
@@ -110,5 +115,32 @@ func (ao *Array) Inspect() string {
 	out.WriteString("[")
 	out.WriteString(strings.Join(elements, ", "))
 	out.WriteString("]")
+	return out.String()
+}
+
+type HashPair struct {
+	Key   Object
+	Value Object
+}
+
+type Hashable interface {
+	HashKey() HashKey
+}
+
+type Hash struct {
+	Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s",
+			pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 	return out.String()
 }
