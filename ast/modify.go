35da0b9bd1ad7f70b703440af32eff244642564a
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-08-03 19:05:09 +0200

    update modify for handling statements

--- ast/modify.go
+++ ast/modify.go
@@ -15,6 +15,32 @@ func Modify(node Node, modifier ModifierFunc) Node {
 	case *InfixExpression:
 		node.Left, _ = Modify(node.Left, modifier).(Expression)
 		node.Right, _ = Modify(node.Right, modifier).(Expression)
+
+	case *PrefixExpression:
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+
+	case *IndexExpression:
+		node.Left, _ = Modify(node.Left, modifier).(Expression)
+		node.Index, _ = Modify(node.Index, modifier).(Expression)
+
+	case *IfExpression:
+		node.Condition, _ = Modify(node.Condition, modifier).(Expression)
+		node.Consequence, _ = Modify(node.Consequence, modifier).(*BlockStatement)
+		if node.Alternative != nil {
+			node.Alternative, _ = Modify(node.Alternative, modifier).(*BlockStatement)
+		}
+	
+	case *BlockStatement:
+		for i, _ := range node.Statements {
+			node.Statements[i], _ = Modify(node.Statements[i], modifier).(Statement)
+		}
+
+	case *LetStatement:
+		node.Value, _ = Modify(node.Value, modifier).(Expression)
+	
+	case *ReturnStatement:
+		node.ReturnValue, _ = Modify(node.ReturnValue, modifier).(Expression)
+
 	}
 	return modifier(node)
 }
