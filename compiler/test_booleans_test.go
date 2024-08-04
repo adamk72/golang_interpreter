f0a5eee120bb8d81d0df5858b399e339cb216a46
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-08-04 18:00:33 +0200

    add in boolean op codes (p69)

---
+++ compiler/test_booleans_test.go
@@ -0,0 +1,27 @@
+package compiler
+
+import (
+	"monkey/code"
+	"testing"
+)
+
+func TestBooleanExpressions(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "true",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpTrue),
+				code.Make(code.OpPop),
+			},
+		}, {
+			input:             "false",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpFalse),
+				code.Make(code.OpPop),
+			},
+		},
+	}
+	runCompilerTests(t, tests)
+}
--- compiler/test_compiler_test.go
+++ compiler/test_compiler_test.go
@@ -16,57 +16,6 @@ type compilerTestCase struct {
 	expectedInstructions []code.Instructions
 }
 
-func TestIntegerArithmetic(t *testing.T) {
-	tests := []compilerTestCase{
-		{
-			input:             "1 + 2",
-			expectedConstants: []interface{}{1, 2},
-			expectedInstructions: []code.Instructions{
-				code.Make(code.OpConstant, 0),
-				code.Make(code.OpConstant, 1),
-				code.Make(code.OpAdd),
-				code.Make(code.OpPop),
-			},
-		}, {
-			input:             "1; 2",
-			expectedConstants: []interface{}{1, 2},
-			expectedInstructions: []code.Instructions{
-				code.Make(code.OpConstant, 0),
-				code.Make(code.OpPop),
-				code.Make(code.OpConstant, 1),
-				code.Make(code.OpPop),
-			}},
-		{
-			input:             "1 - 2",
-			expectedConstants: []interface{}{1, 2},
-			expectedInstructions: []code.Instructions{
-				code.Make(code.OpConstant, 0),
-				code.Make(code.OpConstant, 1),
-				code.Make(code.OpSub),
-				code.Make(code.OpPop),
-			}},
-		{
-			input:             "1 * 2",
-			expectedConstants: []interface{}{1, 2},
-			expectedInstructions: []code.Instructions{
-				code.Make(code.OpConstant, 0),
-				code.Make(code.OpConstant, 1),
-				code.Make(code.OpMul),
-				code.Make(code.OpPop),
-			}},
-		{
-			input:             "2 / 1",
-			expectedConstants: []interface{}{2, 1},
-			expectedInstructions: []code.Instructions{
-				code.Make(code.OpConstant, 0),
-				code.Make(code.OpConstant, 1),
-				code.Make(code.OpDiv),
-				code.Make(code.OpPop),
-			},
-		},
-	}
-	runCompilerTests(t, tests)
-}
 
 func runCompilerTests(t *testing.T, tests []compilerTestCase) {
 	t.Helper()
