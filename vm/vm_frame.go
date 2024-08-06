b31bb024b311babcccea9d620cd3f9ad80269f55
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2024-08-06 17:10:48 +0200

    add frames to stack (p170)

---
+++ vm/vm_frame.go
@@ -0,0 +1,33 @@
+package vm
+
+import (
+	"monkey/code"
+	"monkey/object"
+)
+
+type Frame struct {
+	fn *object.CompiledFunction
+	ip int
+}
+
+func NewFrame(fn *object.CompiledFunction) *Frame {
+	return &Frame{fn: fn, ip: -1}
+}
+
+func (f *Frame) Instructions() code.Instructions {
+	return f.fn.Instructions
+}
+
+func (vm *VM) currentFrame() *Frame {
+	return vm.frames[vm.framesIndex-1]
+
+}
+func (vm *VM) pushFrame(f *Frame) {
+	vm.frames[vm.framesIndex] = f
+	vm.framesIndex++
+}
+
+func (vm *VM) popFrame() *Frame {
+	vm.framesIndex--
+	return vm.frames[vm.framesIndex]
+}
